ed201125e91ecfb938fddeedf14e3a7b9898b0ba
Alexandra Camarena <alexandracamarena@ou.edu>
2024-03-06 11:53:12 -0600

    Add project files.

---
+++ LCP_0306/Program.cs
@@ -0,0 +1,19 @@
+﻿// MIS 3013 001
+// March 6, 2024
+// Alexandra Camarena
+// 113568154
+
+Console.WriteLine("Void fucntion");
+
+// define a function, will print out string for the specified times 
+// void function
+
+void PrintStrings(string str, int times)
+{
+    for (int i = 0; i < times; i = i + 1)
+    {
+        Console.WriteLine(str);
+    }
+}
+
+PrintStrings("MIS3013", 20);
